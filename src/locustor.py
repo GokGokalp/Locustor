6cf443d4c84913e4e54ec25151f27eebcfd7a7d7
Gokhan Gokalp <gokhan.gokalp@trendyol.com>
2017-12-20 17:09:43 +0300

    end date param added

--- src/locustor.py
+++ src/locustor.py
@@ -1,18 +1,36 @@
 'locustor.py'
 
+import sys
+from datetime import datetime, timedelta
 import time
 import schedule
 from locustor_config import LocustorConfig
 from log_repository import LogRepository
 from log_helper import LogHelper
 
-def job():
-    'Locustor is a simple scheduled worker which sends Locust logs to InfluxDB.'
-    log_output = LogHelper().parse_log_to_json(LocustorConfig.LOCUST_CSV_LOG_FILE_PATH)
-    LogRepository().add_log(log_output)
+class Locustor:
+    'Start point'
 
-schedule.every(LocustorConfig.DELAY_IN_SEC).seconds.do(job)
+    def __init__(self):
+        self.end_date = datetime.now() + timedelta(minutes=int(sys.argv[1]))
 
-while True:
+    def job(self):
+        'Locustor is a simple scheduled worker which sends Locust logs to InfluxDB.'
+
+        print(datetime.now())
+        log_output = LogHelper().parse_log_to_json(LocustorConfig.LOCUST_CSV_LOG_FILE_PATH)
+        LogRepository().add_log(log_output)
+
+    def is_runnable(self):
+        'Function is runnable'
+        if self.end_date < datetime.now():
+            return False
+        return True
+
+LOCUSTOR = Locustor()
+
+schedule.every(LocustorConfig.DELAY_IN_SEC).seconds.do(LOCUSTOR.job)
+
+while LOCUSTOR.is_runnable():
     schedule.run_pending()
     time.sleep(1)
